930c539ed24b8bab7b44d5211671f47dc4294a0d
mizher0 <mdmizher@gmail.com>
2025-12-23 12:05:28 +0400

    Create __init__.py

---
+++ models/ensemble/__init__.py
@@ -0,0 +1,4 @@
+# ensemble/__init__.py
+from .ensemble import TemperatureScaling, WeightedEnsemble, HierarchicalEnsemble
+
+__all__ = ['TemperatureScaling', 'WeightedEnsemble', 'HierarchicalEnsemble']
